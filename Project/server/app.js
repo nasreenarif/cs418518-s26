7e369fdd8525eea56e6a192343ced9a91dc7bea0
Nasreen Arif <nasreen_arif@live.com>
2026-02-18 00:53:15 +0000

    chore: initialize server package with express and nodemon dependencies

---
+++ Project/server/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app=express();
+const port=3000;
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is listening at port ${port}`);
+})
+
+
+export default app;
